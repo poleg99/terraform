9746be1b2f426c8fb6153086fbd05ffe70c8d221
Oleg Pavlov <oleg.pavlov@inplatlabs.ru>
2022-02-23 21:18:39 +0300

    add zone_name var

--- epam_aws/main.tf
+++ epam_aws/main.tf
@@ -11,11 +11,13 @@ terraform {
 }
 
 module "epam_task_network" {
-  source = "../modules/network"
+  source    = "../modules/network"
+  zone_name = var.zone_name
 }
 
 module "epam_task_servers" {
   source          = "../modules/servers"
+  zone_name       = var.zone_name
   vpc_id_data     = module.epam_task_network.vpc_id
   db_subnet_name  = module.epam_task_network.db_subnet_name
   nlb_subnets     = module.epam_task_network.public_subnet_ids
